c0d63fd75d09bd2ec331b8169a56b823d1a29198
Makde-gif <makdekiu@gmail.com>
2025-03-22 15:44:30 +0000

    CartaA01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,42 @@
 //Teste larissa
 
 int main() {
-    printf("CartasSuperTrunfo \n");
-    printf("Novo commit \n");
+    char estado[3];
+    char codigo[4];
+    char nome_da_cidade[20];
+    int populacao = 11895578 ;
+    float area_em_km2 = 1.52111;
+    float PIB = 7146;
+    int numero_de_pontos_turisticos = 10;
+
+    printf("Código da carta: ");;
+    scanf("%3s", codigo);
+    printf("Código: %s\n", codigo);
+
+    printf("Estado: ");
+    scanf("%2s", estado);
+    printf("Estado: %s\n", estado);
+
+    printf("Nome da cidade: ");
+    scanf("%20s", nome_da_cidade);
+    printf("Cidade: %s\n", nome_da_cidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+    printf("População: %d\n", populacao);
+
+    printf("Área em km²: ");
+    scanf("%f", &area_em_km2);
+    printf("Área: %.2f km²\n", area_em_km2);
+
+    printf("PIB: ");
+    scanf("%f", &PIB);
+    printf("PIB: %.2f\n", PIB);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &numero_de_pontos_turisticos);
+    printf("Pontos turísticos: %d\n", numero_de_pontos_turisticos);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
